d41f0702a138a03dbac0f9a7790c7905a0d9895e
Usman Liaqat <chusmanliaqat1020@gmail.com>
2023-01-30 12:35:03 +0500

    Initial Commit

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,46 +1,43 @@
 import { HttpException, HttpStatus, Injectable, NotFoundException } from "@nestjs/common";
 import { Coffee } from "./entities/coffee.entity";
+import { CreateCoffeeDto } from "./dto/create-coffee.dto/create-coffee.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: "Shipwreck Roast",
-      brand: "Buddy Brew",
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
 
-  findAll() {
-    return this.coffees;
+  constructor(@InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>) {
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find(item => item.id === +id);
-    if(!coffee) {
-    throw new NotFoundException(`Coffee #${id} not found`)
+  findAll() {
+    return this.coffeeModel.find().exec();
+  }
+
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({_id: id}).exec();
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
     }
-    return coffee
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
-    return  this.coffees.push(createCoffeeDto);
+    const coffee = new this.coffeeModel(createCoffeeDto);
+    return coffee.save();
   }
 
   update(id: string, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
-    // @ts-ignore
-    if(existingCoffee) {
+    if (existingCoffee) {
       //Update the existing entity
-
     }
   }
 
   remove(id: string) {
     const coffeeIndex = this.coffees.findIndex(item => item.id === +id);
-    if(coffeeIndex >= 0) {
-      return this.coffees.splice(coffeeIndex, 1)
+    if (coffeeIndex >= 0) {
+      return this.coffees.splice(coffeeIndex, 1);
     }
   }
 }
